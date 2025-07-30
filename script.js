eff7ce22fde46ab3ac374b1654b0640043c787f0
awrmurphy <awrmurphy@gmail.com>
2025-07-30 18:08:52 -0400

    Addition of side nav, css and js

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 //js will handle local storage and dynamic css changes like light mode/dark mode
 //and resizing of new post form on focus/blur
+function openNav() {
+  document.getElementById("sideNav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("sideNav").style.width = "0";
+}
+
