57aa3f17f331a16f4293e020f3f1fce47412c769
awrmurphy <awrmurphy@gmail.com>
2025-07-31 18:37:37 -0400

    added edit functionality

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //js will handle local storage and dynamic css changes like light mode/dark mode
 //and resizing of new post form on focus/blur
 let postArray = [];
+let postHistory;
 
 function openNav() {
   document.getElementById("sideNav").style.width = "250px";
@@ -85,9 +86,7 @@ function post(){
     localStorage.setItem("UserPost",loggedInUser);
     localStorage.setItem("postTitle",document.getElementById("postTitle").value);
     localStorage.setItem("postContent",document.getElementById("postContent").value);
-    if(document.getElementById("postImg").value !=null){
-
-}
+   
     location.href = "user.html";
 }
 
@@ -99,7 +98,9 @@ function loadHistory(){
     let postContent = localStorage.getItem("postContent");
     localStorage.removeItem("postContent");
     let postImg;
-        
+        if(localStorage.getItem("posts")!=null){
+            postArray = JSON.parse(localStorage.getItem("posts"));
+        }
     if(localStorage.getItem("postImg")!=undefined){
     postImg = localStorage.getItem("postImg");
     localStorage.removeItem("postImg");
@@ -109,53 +110,115 @@ function loadHistory(){
         postC : postContent,
         postI : postImg
     };
-    console.log(postArray);
-    
-    }else{
+    }
+    else
+        {
         postArray[postArray.length] = {
         loggedUser : loggedInUser,
         postN : postName,
         postC : postContent
         }
-        console.log(postArray);
         
-    }
-    for(let i =0;i<postArray.length;){
+        }
+
+    for(let i =postArray.length-1;i>=0;){
         buildPost(i);
-        i++;
+        i--;
     }
+    postHistory = JSON.stringify(postArray);
     
+    localStorage.setItem("posts",postHistory);
 }
 
 function buildPost(i){
     const post = postArray[i];
-
+    
     const br = document.createElement('br');
     const container = document.createElement('div');
     container.setAttribute('class','oldPost');
     container.setAttribute('background-color', 'var(--lightModeAccents)');
     container.setAttribute('border-radius', '8px');
     container.setAttribute('width','50%');
+    const postNumber = document.createElement('h1');
+    postNumber.setAttribute('name','postNumber');
+    postNumber.setAttribute('color','var(--darkModeColour');
+    postNumber.innerHTML= (postArray.length-i);
     const postHeader = document.createElement('h1');
     postHeader.setAttribute('color','var(--darkModeColour)')
-    postHeader.innerHTML= (document.getElementsByClassName('oldPost').length+1) + ') ' +post.loggedUser+' says...';
+    postHeader.setAttribute('id',`postHeader: ${postArray.length-i}`);
+    postHeader.innerHTML=') ' +post.loggedUser+' says...';
     postHeader.appendChild(br);
     const postName = document.createElement('h2');
     postName.setAttribute('color', 'var(--darkModeColour)');
+    postName.setAttribute('id',`postName: ${postArray.length-i}`);
     postName.innerHTML=post.postN;
     postName.appendChild(br);
     const postBody = document.createElement('p');
     postBody.setAttribute('color', 'var(--darkModeColour)');
+    postBody.setAttribute('id',`postBody: ${postArray.length-i}`);
     postBody.innerHTML= post.postC;
+    container.appendChild(postNumber);
     container.appendChild(postHeader);
     container.appendChild(postName);
     container.appendChild(postBody);
     if(post.postI!=undefined){
         const postImage = document.createElement('img');
         postImage.setAttribute('src',post.postI);
-        postImage.appendChild(br);
+        postImage.setAttribute('id',`postImage: ${postArray.length-i}`);
         container.appendChild(postImage);
+        container.appendChild(br);
     }
+    const edit = document.createElement('button');
+    edit.setAttribute('onclick','edit(this.id)');
+    edit.setAttribute('id',postArray.length-i);
+    edit.innerHTML='Edit Post';
+    container.appendChild(edit);
+    const del = document.createElement('button');
+    del.setAttribute('onclick','del(this.id)');
+    del.setAttribute('id',postArray.length-i)
+    del.innerHTML='Delete Post';
+    container.appendChild(del);
     document.body.appendChild(container);
+}
+
+function edit(i){
+    document.getElementById(`postName: ${i}`).outerHTML=`<input type="text" placeholder="Post Title" id="postName: ${i}" minlength="1" maxlength="50" required>`;
+    document.getElementById(`postBody: ${i}`).outerHTML=`<textarea id="postBody: ${i}" minlength="1" maxlength="200" cols="25" rows="4" required></textarea>`;
+    if(document.getElementById(`postImage: ${i}`)){
+    document.getElementById(`postImage: ${i}`).outerHTML=`<input type="file" id="postImage: ${i}" name="filename" accept="image/*" onchange="read(this)"><br>`;
+    }
+    document.getElementById(i).outerHTML=`<button id=${i} onclick="editPost(this.id)">Save</button>`
+}
+
+function editPost(i){
+    console.log(i);
+    console.log(postArray);
+    
+    
+    var thisPost = postArray[i];
+    console.log(thisPost);
+    
+    
+    thisPost.postN = document.getElementById(`postName: ${i}`).value;
+    thisPost.postC = document.getElementById(`postBody: ${i}`).value;
+    if(document.getElementById(`postImage: ${i}`)){
+        thisPost.postI = localStorage.getItem('postImg');
+    }
+    postArray[i]=thisPost;
+    postHistory = JSON.stringify(postArray);
+    localStorage.setItem('posts', postHistory);
+    document.getElementById(`postName: ${i}`).outerHTML=`<h2 color="var(--darkModeColour)" id='postName: ${i}'></h2>`;
+    document.getElementById(`postName: ${i}`).innerHTML=thisPost.postN
+    document.getElementById(`postBody: ${i}`).outerHTML=`<p color="var(--darkModeColour)" id="postBody: ${i}"></p>`;
+    document.getElementById(`postBody: ${i}`).innerHTML=thisPost.postC
+    if(document.getElementById(`postImage: ${i}`)){
+        document.getElementById(`postImage: ${i}`).outerHTML=`<img id='postImage: ${i}'></img>`;
+        document.getElementById(`postImage: ${i}`).setAttribute('src',thisPost.postI);
+    }
+
+}
+
+function del(i){
+
 }
 
