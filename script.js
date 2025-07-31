74587412c862c256d2c3b2fb0ad79c81f4b86aa3
awrmurphy <awrmurphy@gmail.com>
2025-07-30 23:31:24 -0400

    implemented new post creating post element on post history page.

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ function newPost() {
     loggedInUser=localStorage.getItem("UserPost");
     localStorage.removeItem("UserPost");
     if(loggedInUser != undefined){
+        document.getElementById("newPoster").innerHTML=loggedInUser+" says...";
         document.getElementById("welcomeMessage").style.visibility = "visible";
         document.getElementById("welcomeMessage").innerHTML= "Welcome "+loggedInUser+"!";
         document.getElementById("loginMessage").style.visibility = "hidden";
@@ -63,4 +64,37 @@ function logout(){
 function passUser(){
     localStorage.setItem("UserPost", loggedInUser);
     location.href = "newpost.html";
+}
+
+function post(){
+    event.preventDefault();
+    localStorage.setItem("UserPost",loggedInUser);
+    localStorage.setItem("postContent",document.getElementById("postContent").value);
+    location.href = "user.html";
+}
+
+function loadHistory(){
+    loggedInUser=localStorage.getItem("UserPost");
+    localStorage.removeItem("UserPost");
+    let postContent = localStorage.getItem("postContent");
+    localStorage.removeItem("postContent");
+    savePost(postContent);
+}
+
+function savePost(string){
+    const br = document.createElement('br');
+    const container = document.createElement('div');
+    container.setAttribute('class','oldPost');
+    container.setAttribute('background-color', 'var(--lightModeAccents)');
+    container.setAttribute('border-radius', '8px');
+    const postHeader = document.createElement('h1');
+    postHeader.setAttribute('color','var(--darkModeColour)')
+    postHeader.innerHTML= (document.getElementsByClassName('oldPost').length+1) + ') ' +loggedInUser+' says...';
+    postHeader.appendChild(br);
+    const postBody = document.createElement('p');
+    postBody.setAttribute('color', 'var(--darkModeColour)');
+    postBody.innerHTML= string;
+    container.appendChild(postHeader);
+    container.appendChild(postBody);
+    document.body.appendChild(container);
 }
